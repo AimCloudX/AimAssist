3beb2aa85b451146300bef49f21832891b476cec
minamihirata1234 <minami.hirata@aimnext.com>
2024-05-30 13:48:47 +0900

    JSON形式の変換機能作成

--- src/Plugins/ClipboardAnalyzer/UI/ClipboardList.xaml.cs
+++ src/Plugins/ClipboardAnalyzer/UI/ClipboardList.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -38,12 +39,56 @@ namespace ClipboardAnalyzer
                 Items.Add(clipboardData);
             }
             this.DataContext = this;
-            this.ComboBox.ItemsSource = clipboardDatas.Select(x => x.Format).ToList();
+
+            var formats = Items.Select(x => x.Format).ToList();
+            formats.Add("JSON");
+
+            this.ComboBox.ItemsSource = formats;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Clipboard.SetText(this.PreviewText.Text);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedText = this.ComboBox.SelectedItem as string;
+
+            foreach(var item in Items)
+            {
+                if(item.Format == selectedText)
+                {
+                    this.PreviewText.Text = item.Data.ToString();
+                }
+            }
+
+            if(selectedText == "JSON")
+            {
+                string text = "";
+                foreach (var item in Items)
+                {
+                    if (item.Format == "Text")
+                    {
+                        text = item.Data.ToString();
+                    }
+                }
+
+                if(string.IsNullOrEmpty(text))
+                {
+                    return;
+                }
+
+                string[] result = text.Split('\t');
+                if (result.Length == 2)
+                {
+                    var key = result[0];
+                    var value = result[1].Replace("\r\n", "");
+                    this.PreviewText.Text = $"{{\"{key}\" : \"{value}\"}}";
+                }
+
+                
+            }
         }
     }
 }
