ddf39634861be426357a3e00c59aa47cfefcd5d9
shigekazukoya <shigekazukoya@gmail.com>
2024-06-10 00:01:42 +0900

    AimEditorの追加

---
+++ src/AimAssist.Core/Editors/FileModel.cs
@@ -0,0 +1,32 @@
+﻿using AimAssist.Core.Options;
+using Common.UI.Editor;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AimAssist.Core.Editors
+{
+    public class FileModel : TabItem
+    {
+        public FileModel(string filePath)
+        {
+            this.FileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            this.FilePath = filePath;
+            this.monacoEditor = new MonacoEditor();
+            this.monacoEditor.SetOption(EditorOptionService.Option);
+            this.monacoEditor.SetText(File.ReadAllText(filePath));
+            this.Content = this.monacoEditor;
+
+            // style
+            var textblock = new TextBlock();
+            textblock.Text = this.FileNameWithoutExtension;
+            this.Header = textblock;
+
+        }
+
+        public string FileNameWithoutExtension { get; set; }
+        public string FilePath { get; set; }
+
+        public MonacoEditor monacoEditor { get; set; }
+    }
+}
--- src/AimAssist.Core/Options/EditorOptionService.cs
+++ src/AimAssist.Core/Options/EditorOptionService.cs
@@ -4,9 +4,10 @@ using System.IO;
 
 namespace AimAssist.Core.Options
 {
-    public static class EditorOptionService
+    public class EditorOptionService
     {
         public static EditorOption Option;
+        private static FileSystemWatcher watcher;
 
         public static string OptionPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "editor.option.json");
 
@@ -24,7 +25,26 @@ namespace AimAssist.Core.Options
                 Option = option;
                 SaveOption();
             }
+
+            watcher = new FileSystemWatcher(Path.GetDirectoryName(EditorOptionService.OptionPath));
+                // 監視する変更タイプを設定
+                watcher.NotifyFilter = NotifyFilters.FileName
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.LastAccess
+                                     | NotifyFilters.CreationTime;
+
+                // 監視するファイルの種類を指定
+                watcher.Filter = Path.GetFileName(EditorOptionService.OptionPath);
+
+                // イベントハンドラを追加
+                watcher.Changed += OnChanged;
+
+                // 監視を開始
+                watcher.EnableRaisingEvents = true;
         }
+
+        private static void OnChanged(object source, FileSystemEventArgs e) =>
+            EditorOptionService.LoadOption();
 
         public static void SaveOption()
         {
