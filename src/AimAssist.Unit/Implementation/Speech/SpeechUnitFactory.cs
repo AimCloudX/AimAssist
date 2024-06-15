cef9ac45f1f1f6d8e9d32816bd68c0b0ada765f7
shigekazukoya <shigekazukoya@gmail.com>
2024-06-15 19:09:35 +0900

    TempUnit Voskによる文字起こし

---
+++ src/AimAssist.Unit/Implementation/Speech/SpeechUnitFactory.cs
@@ -0,0 +1,39 @@
+﻿using AimAssist.Unit.Core;
+using AimAssist.Unit.Core.Mode;
+using AimAssist.Unit.Implementation.WorkFlows;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace AimAssist.Unit.Implementation.Speech
+{
+    public class SpeechUnitFactory : IUnitsFacotry
+    {
+        public IPickerMode TargetMode => WorkFlowMode.Instance;
+
+        public bool IsShowInStnadard => true;
+
+        public async IAsyncEnumerable<IUnit> GetUnits(UnitsFactoryParameter pamater)
+        {
+            yield return new SpeechUnit();
+        }
+    }
+
+    public class SpeechUnit : IUnit
+    {
+        public BitmapImage Icon => new BitmapImage();
+
+        public IPickerMode Mode => WorkFlowMode.Instance;
+
+        public string Category => "";
+
+        public string Name => "Real-time Transcription";
+
+        public string Text => "文字起こし";
+
+        public UIElement GetUiElement()
+        {
+            return new SpeechControl();
+
+        }
+    }
+}
