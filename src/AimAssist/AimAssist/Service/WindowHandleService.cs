5ba93ef74af6481d0af25d82ee2fb688a9d59395
shigekazukoya <shigekazukoya@gmail.com>
2024-08-07 00:10:32 +0900

    MainWindowのToggle時のWindowHandle修正

--- src/AimAssist/AimAssist/Service/WindowHandleService.cs
+++ src/AimAssist/AimAssist/Service/WindowHandleService.cs
@@ -7,13 +7,10 @@ namespace AimAssist.Service;
 internal static class WindowHandleService
 {
     public static MainWindow Window { get; private set; } = new MainWindow();
-    private static IntPtr beforeWindow;
     private static bool isActivate;
 
     [DllImport("user32.dll")]
     private static extern bool SetForegroundWindow(IntPtr hWnd);
-    [DllImport("user32.dll")]
-    static extern IntPtr GetForegroundWindow();
 
     public static void ToggleMainWindow()
     {
@@ -26,11 +23,6 @@ internal static class WindowHandleService
         }
 
         isActivate = true;
-        // HotKey押下時のウィンドウのハンドルを取得
-        beforeWindow = GetForegroundWindow();
-
-        // 自身のウィンドウハンドルをアクティブにする
-        SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
 
         if (Window.IsClosing)
         {
@@ -39,37 +31,15 @@ internal static class WindowHandleService
 
         Window.Visibility = System.Windows.Visibility.Visible;
         Window.Closed += DoAction;
+        Window.Focus();
         Window.Show();
+         
+        // 自身のウィンドウハンドルをアクティブにする
+        SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
     }
 
     private static void DoAction(object? sender, EventArgs e)
     {
-        try
-        {
-            isActivate = false;
-            if (sender is MainWindow window)
-            {
-                var text = window.SnippetText;
-                if (string.IsNullOrEmpty(text))
-                {
-                    // 元のプロセスをアクティブにする
-                    SetForegroundWindow(beforeWindow);
-                    return;
-                }
-
-                System.Windows.Clipboard.SetText(text);
-            }
-        }
-        catch (Exception ex)
-        {
-            System.Windows.MessageBox.Show(ex.Message);
-        }
-
-        // 元のプロセスをアクティブにする
-        SetForegroundWindow(beforeWindow);
-        Thread.Sleep(100); // アクテイブになるまで少し待つ
-
-        // SendKeysを使用してキーを送信するためにSystem.Windows.Formsを追加する必要がある
-        SendKeys.SendWait("^v");
+        isActivate = false;
     }
 }
