7a49651df0005d87ea0be814d9e371e85e5d4678
shigekazukoya <shigekazukoya@gmail.com>
2025-06-08 22:02:41 +0900

    Commandsのページ追加

--- src/AimAssist/AimAssist.Unit/Implementation/Commands/AimAssistCommandsControl.xaml.cs
+++ src/AimAssist/AimAssist.Unit/Implementation/Commands/AimAssistCommandsControl.xaml.cs
@@ -1,23 +1,65 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 using AimAssist.Core.Attributes;
 using AimAssist.Core.Interfaces;
+using Common.UI.Commands;
 
 namespace AimAssist.Units.Implementation.Apps;
 
 [AutoDataTemplate(typeof(AimAssistCommandsUnit))]
 public partial class AimAssistCommandsControl : UserControl
 {
-    private readonly IAppCommands appCommands;
+    private readonly ICommandService commandService;
 
-    public AimAssistCommandsControl(IAppCommands appCommands)
+    public AimAssistCommandsControl(ICommandService commandService)
     {
-        this.appCommands = appCommands;
+        this.commandService = commandService;
         InitializeComponent();
+        CreateCommandButtons();
     }
 
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    private void CreateCommandButtons()
     {
-        appCommands.ShutdownAimAssist.Execute(this);
+        var commandProperties = commandService.GetKeymap()
+            .ToList();
+
+        foreach (var property in commandProperties)
+        {
+            if (commandService.TryGetCommand(property.Key, out var command))
+            {
+                if (command.CanExecute(this))
+                {
+                    var stackPanel = new StackPanel(){Orientation = Orientation.Horizontal};
+                    var button = new Button
+                    {
+                        Content = property.Key,
+                        Margin = new Thickness(0, 0, 0, 5),
+                        Padding = new Thickness(10, 5, 10, 5),
+                        Tag = command
+                    };
+                    
+                    stackPanel.Children.Add(button);
+
+                    if (property.Value != null)
+                    {
+                        var textBlock  = new TextBlock(){Text = property.Value.ToString()};
+                        textBlock.Margin = new Thickness(20, 0, 0, 0);
+                        stackPanel.Children.Add(textBlock);
+                    }
+                    
+                    button.Click += CommandButton_Click;
+                    CommandButtonsPanel.Children.Add(stackPanel);
+                }
+            }
+        }
+    }
+
+    private void CommandButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is RelayCommand command)
+        {
+            command.Execute(this);
+        }
     }
 }
