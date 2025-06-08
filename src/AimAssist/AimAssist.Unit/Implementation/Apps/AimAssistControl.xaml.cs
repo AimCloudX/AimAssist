99c65607572e00230e7009c38bc2a2bafbf69d8a
shigekazukoya <shigekazukoya@gmail.com>
2025-06-08 21:04:25 +0900

    AimAssitの操作画面追加

---
+++ src/AimAssist/AimAssist.Unit/Implementation/Apps/AimAssistControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using AimAssist.Core.Attributes;
+using AimAssist.Core.Interfaces;
+
+namespace AimAssist.Units.Implementation.Apps;
+
+[AutoDataTemplate(typeof(AppUnit),true)]
+public partial class AimAssistControl : UserControl
+{
+    private readonly IAppCommands appCommands;
+
+    public AimAssistControl(IAppCommands appCommands)
+    {
+        this.appCommands = appCommands;
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        appCommands.ShutdownAimAssist.Execute(this);
+    }
+}
---
+++ src/AimAssist/AimAssist.Unit/Implementation/Apps/AppUnit.cs
@@ -0,0 +1,13 @@
+﻿using AimAssist.Core.Attributes;
+using AimAssist.Core.Units;
+
+namespace AimAssist.Units.Implementation.Apps;
+
+[AutoRegisterUnit("AimAssist", Priority = 80)]
+public class AppUnit : IUnit
+{
+    public IMode Mode { get; } = WorkTools.WorkToolsMode.Instance;
+    public string Name { get; } = "AimAssist";
+    public string Description { get; } = "AimAssist";
+    public string Category { get; } = "AimAssist";
+}
